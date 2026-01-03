b3a5c7966414eca79b417003165970a78708e567
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2026-01-03 13:21:55 +0530

    Bump Python SDK version to 0.1.1

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engram-sdk",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "grpcio>=1.76.0",
